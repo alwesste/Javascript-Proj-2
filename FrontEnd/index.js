c923cc4f7e4f1239a6a5f0ed616e1fc1e29e5525
Leopold <leopolddag@gmail.com>
2023-04-22 17:21:29 +0200

    first commit

---
+++ FrontEnd/index.js
@@ -0,0 +1,28 @@
+const works = await fetch("http://localhost:5678/api/works").then(works => works.json())
+console.log(works)
+
+
+works.map(work => {
+    // const diapo = works[i]
+
+    const image = document.createElement("img");
+    image.src = work.imageUrl;
+    const figCaption = document.createElement("figcaption");
+    figCaption.innerText = work.title;
+
+
+    const figure = document.createElement("figure");
+    const gallery = document.querySelector(".gallery");
+    figure.appendChild(image);
+    figure.appendChild(figCaption);
+    gallery.appendChild(figure);
+
+    console.log(image)
+});
+
+    
+
+
+
+   
+

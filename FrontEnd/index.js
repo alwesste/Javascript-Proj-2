0acc588ca1685e844245b016bc77d4179e95d27f
Leopold <leopolddag@gmail.com>
2023-04-23 14:50:51 +0200

    set objetct adding

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,7 +1,10 @@
 const works = await fetch("http://localhost:5678/api/works").then(works => works.json())
 
-function genererWorks(works) {
-    works.map(work => {
+const mySet = new Set(works)
+const uniqueWorks = [...mySet]
+
+function genererWorks(uniqueWorks) {
+  uniqueWorks.map(work => {
     const image = document.createElement("img");
     image.src = work.imageUrl;
     const figCaption = document.createElement("figcaption");
@@ -10,39 +13,39 @@ function genererWorks(works) {
     const gallery = document.querySelector(".gallery");
     figure.appendChild(image);
     figure.appendChild(figCaption);
-    gallery.appendChild(figure);   
+    gallery.appendChild(figure);     
     });
 }
 
-genererWorks(works)
+genererWorks(uniqueWorks)
 
 
 
-const filter_tous = document.querySelector(".filter_tous")
-filter_tous.addEventListener("click", function() {
-    const ordered_tous = works;
+const filter_all = document.querySelector(".filter_all")
+filter_all.addEventListener("click", function() {
+    const ordered_all = uniqueWorks;
     document.querySelector(".gallery").innerHTML = "";
-    genererWorks(ordered_tous)
+    genererWorks(ordered_all)
 
 })
 
 const filter_objets = document.querySelector(".filter_objets");
 filter_objets.addEventListener("click", function() {
-    const orderedListObjet = works.filter(work => work.category.id == 1)
+    const orderedListObjet = uniqueWorks.filter(work => work.category.id == 1)
     document.querySelector(".gallery").innerHTML = "";
     genererWorks(orderedListObjet)
 });
 
 const filter_appartements = document.querySelector(".filter_appartements");
 filter_appartements.addEventListener("click", function () {
-    const orderedListAppartements = works.filter(work => work.category.id == 2)
+    const orderedListAppartements = uniqueWorks.filter(work => work.category.id == 2)
     document.querySelector(".gallery").innerHTML = "";
     genererWorks(orderedListAppartements)
 })
 
 const filter_hotels_restaurants = document.querySelector(".filter_hotels_restaurants")
 filter_hotels_restaurants.addEventListener("click", function () {
-    const ordered_hotels_restaurants = works.filter(work => work.category.id == 3)
+    const ordered_hotels_restaurants = uniqueWorks.filter(work => work.category.id == 3)
     document.querySelector(".gallery").innerHTML = "";
     genererWorks(ordered_hotels_restaurants)
 })
